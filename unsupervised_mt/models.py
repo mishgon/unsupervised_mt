1ec2edf222e975ef8a09f8914337c94424d3b995
migonch <misha-goncharov1998@yandex.ru>
2018-11-18 01:56:12 +0300

    not tested trainer

--- unsupervised_mt/models.py
+++ unsupervised_mt/models.py
@@ -7,21 +7,21 @@ def identity(input):
     return input
 
 
-class PretrainedEmbedding(nn.Module):
+class Embedding(nn.Module):
     def __init__(self, emb_matrix):
-        super(PretrainedEmbedding, self).__init__()
+        super(Embedding, self).__init__()
         self.embedding = nn.Embedding.from_pretrained(torch.from_numpy(emb_matrix))
         self.embedding.requires_grad = False
-        self.input_size = self.embedding.num_embeddings
+        self.vocab_size = self.embedding.num_embeddings
         self.embedding_dim = self.embedding.embedding_dim
 
     def forward(self, input):
         return self.embedding(input)
 
 
-class EncoderRNN(nn.Module):
-    def __init__(self, embedding: PretrainedEmbedding, rnn):
-        super(EncoderRNN, self).__init__()
+class Encoder(nn.Module):
+    def __init__(self, embedding: Embedding, rnn):
+        super(Encoder, self).__init__()
         assert embedding.embedding_dim == rnn.input_size
 
         self.embedding = embedding
@@ -35,47 +35,64 @@ class EncoderRNN(nn.Module):
         outputs: length x batch_size x hidden_size
         hidden: n_layers x batch_size x hidden_size
         """
-        embedded = self.embedding(inputs)
-        outputs, hidden = self.rnn(embedded)
+        outputs, hidden = self.rnn(self.embedding(inputs))
         return outputs, hidden
 
 
 class DecoderHat(nn.Module):
-    def __init__(self, hidden_size, output_size):
+    def __init__(self, hidden_size, vocab_size):
         super(DecoderHat, self).__init__()
         self.hidden_size = hidden_size
-        self.output_size = output_size
-        self.linear = nn.Linear(hidden_size, output_size)
+        self.vocab_size = vocab_size
+        self.linear = nn.Linear(hidden_size, vocab_size)
         self.softmax = nn.LogSoftmax(dim=-1)
 
     def forward(self, input):
         return self.softmax(self.linear(input))
 
 
-class DecoderRNN(nn.Module):
-    def __init__(self, embedding: PretrainedEmbedding, rnn, hat: DecoderHat,
-                 max_encoder_length, sos_index, eos_index,
-                 use_cuda=False, use_attention=True):
-        super(DecoderRNN, self).__init__()
-        assert embedding.embedding_dim == rnn.input_size and\
-               embedding.input_size == hat.output_size
+class Attention(nn.Module):
+    def __init__(self, embedding_dim, hidden_size, max_length):
+        """
+        input: batch_size x vocab_size
+        hidden: n_layers x batch_size x hidden_size
+        encoder_outputs: max_length (<= max_length) x batch_size x hidden_size
+        attn_weights: batch_size x max_length
+        attn_applied: batch_size x 1 x hidden_size
+
+        See figure from https://pytorch.org/tutorials/intermediate/seq2seq_translation_tutorial.html
+        """
+        super(Attention, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.hidden_size = hidden_size
+        self.max_length = max_length
+
+        self.attn = nn.Linear(self.hidden_size + self.embedding_dim, self.max_length, bias=False)
+        self.attn_softmax = nn.Softmax(dim=-1)
+        self.attn_combine = nn.Linear(self.embedding_dim + self.hidden_size, self.embedding_dim, bias=False)
+        self.attn_relu = nn.ReLU()
+
+    def forward(self, embedded, hidden, encoder_outputs):
+        attn_weights = self.attn_softmax(self.attn(torch.cat((hidden[-1], embedded), dim=1))).unsqueeze(1)
+        length = encoder_outputs.size(0)
+        attn_applied = torch.bmm(attn_weights[:, :, :length], encoder_outputs.transpose(0, 1))
+        return self.attn_relu(self.attn_combine(torch.cat((embedded, attn_applied.squeeze(1)), 1)))
+
+
+class Decoder(nn.Module):
+    def __init__(self, embedding: Embedding, attention, rnn, hat: DecoderHat, use_cuda=False):
+        super(Decoder, self).__init__()
+        assert embedding.embedding_dim == rnn.input_size and \
+               embedding.vocab_size == hat.vocab_size and \
+               attention.embedding_dim == embedding.embedding_dim
 
         self.embedding = embedding
         self.embedding_dim = embedding.embedding_dim
+        self.attention = attention
         self.rnn = rnn
         self.hidden_size = rnn.hidden_size
         self.hat = hat
-        self.max_encoder_length = max_encoder_length
-        self.sos_index = sos_index
-        self.eos_index = eos_index
         self.use_cuda = use_cuda
-        self.use_attention = use_attention
-
-        if self.use_attention:
-            self.attn = nn.Linear(self.hidden_size + self.embedding_dim, self.max_encoder_length, bias=False)
-            self.attn_softmax = nn.Softmax(dim=-1)
-            self.attn_combine = nn.Linear(self.embedding_dim + self.hidden_size, self.embedding_dim, bias=False)
-            self.attn_relu = nn.ReLU()
 
     def step(self, input, hidden, encoder_outputs):
         """
@@ -83,37 +100,25 @@ class DecoderRNN(nn.Module):
         hidden: n_layers x batch_size x hidden_size
         encoder_outputs: max_length (<= max_length) x batch_size x hidden_size
         embedded: batch_size x embedding_dim
-        attn_weights: batch_size x max_length
-        attn_applied: batch_size x 1 x hidden_size
         rnn_input: batch_size x embedding_dim
         output: 1 x batch_size x hidden_size
-
-        See figure from https://pytorch.org/tutorials/intermediate/seq2seq_translation_tutorial.html
         """
         embedded = self.embedding(input)
-
-        if self.use_attention:
-            attn_weights = self.attn_softmax(self.attn(torch.cat((hidden[-1], embedded), dim=1))).unsqueeze(1)
-            encoder_length = encoder_outputs.size(0)
-            attn_applied = torch.bmm(attn_weights[:, :, :encoder_length], encoder_outputs.transpose(0, 1))
-            rnn_input = self.attn_relu(self.attn_combine(torch.cat((embedded, attn_applied.squeeze(1)), 1)))
-        else:
-            rnn_input = embedded
-
+        rnn_input = self.attention(embedded, hidden, encoder_outputs) if self.attention else embedded
         output, hidden = self.rnn(rnn_input.unsqueeze(0), hidden)
         output = self.hat(output.squeeze(0))
         return output, hidden
 
-    def init_input(self, batch_size):
-        initial_input = torch.full((batch_size,), self.sos_index, dtype=torch.long)
+    def init_input(self, batch_size, sos_index):
+        initial_input = torch.full((batch_size,), sos_index, dtype=torch.long)
         initial_input = initial_input.cuda() if self.use_cuda else initial_input
         return initial_input
 
-    def forward(self, hidden, encoder_outputs, targets, teacher_forcing_ratio=0.5):
+    def forward(self, hidden, encoder_outputs, targets, sos_index, teacher_forcing_ratio=0.5, eos_index=None):
         """
         targets: target_length x batch_size
         """
-        input = self.init_input(encoder_outputs.size(1))
+        input = self.init_input(encoder_outputs.size(1), sos_index)
         outputs = []
         for t in range(targets.size(0)):
             output, hidden = self.step(input, hidden, encoder_outputs)
@@ -127,22 +132,20 @@ class DecoderRNN(nn.Module):
 
 
 class Seq2Seq(nn.Module):
-    def __init__(self, encoder_embedding: PretrainedEmbedding, encoder_rnn,
-                 decoder_embedding: PretrainedEmbedding, decoder_rnn, decoder_hat: DecoderHat,
-                 max_encoder_length, sos_index, eos_index,
-                 use_cuda=False, use_attention=True):
+    def __init__(self, encoder_embedding: Embedding, encoder_rnn,
+                 decoder_embedding: Embedding, attention, decoder_rnn, decoder_hat: DecoderHat,
+                 use_cuda=False):
         super(Seq2Seq, self).__init__()
-        assert encoder_rnn.hidden_size == decoder_rnn.hidden_size and\
+        assert encoder_rnn.hidden_size == decoder_rnn.hidden_size and \
                encoder_rnn.num_layers == decoder_rnn.num_layers
 
-        self.encoder = EncoderRNN(encoder_embedding, encoder_rnn)
-        self.decoder = DecoderRNN(decoder_embedding, decoder_rnn, decoder_hat,
-                                  max_encoder_length, sos_index, eos_index,
-                                  use_cuda, use_attention)
+        self.encoder = Encoder(encoder_embedding, encoder_rnn)
+        self.decoder = Decoder(decoder_embedding, attention, decoder_rnn, decoder_hat, use_cuda)
 
-    def forward(self, inputs, targets, teacher_forcing_ratio=0.5):
+    def forward(self, inputs, targets, sos_index, teacher_forcing_ratio=0.5, eos_index=None):
         encoder_outputs, hidden = self.encoder(inputs)
-        decoder_outputs = self.decoder(hidden, encoder_outputs, targets, teacher_forcing_ratio)
+        decoder_outputs = self.decoder(hidden, encoder_outputs, targets,
+                                       sos_index, teacher_forcing_ratio, eos_index)
         return decoder_outputs, encoder_outputs
 
 
@@ -157,6 +160,9 @@ class Discriminator(nn.Module):
             nn.Linear(10, 1)
         )
 
-    def forward(self, encoder_output):
-        return self.layers(encoder_output)
+    def forward(self, encoder_outputs):
+        outputs = []
+        for t in range(encoder_outputs.size(0)):
+            outputs.append(self.layers(encoder_outputs[t]).unsqueeze(0))
 
+        return torch.cat(outputs)
